e100e5ec3414d5b606fb248b974d6de9b9861bd8
carlos676767 <carlitobr730@gmail.com>
2024-03-15 08:05:29 -0700

    validao no inpu colocada

--- rastreador/src/main.ts
+++ rastreador/src/main.ts
@@ -1,15 +1,9 @@
 
-const input = document.querySelector("input")  as HTMLInputElement
-const button = document.querySelector("button") as HTMLButtonElement
-const imagem = document.querySelector("img") as HTMLImageElement
-
-
 const converterTempereatura = (data: any) => {
     const p = document.querySelector("p") as HTMLParagraphElement
     const kelvin = data.main.temp
     const celsus = kelvin - 273.15
     p.innerHTML = celsus.toFixed(2)
-    
 }
 
 const obterPaisENomeCidade = (data: any) => {
@@ -21,24 +15,35 @@ const obterPaisENomeCidade = (data: any) => {
 
 
 const obterIcone = (data: any) => {
+  const imagem = document.querySelector("img") as HTMLImageElement
   const iconCode = data
   imagem.src = iconCode
   console.log(iconCode);
   return data
 }
 
+
+const validaoInput = (city:string) => {
+  return city === ''
+}
+
 const gerarFrase = () => {
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=af1f563fdce202604e28dada8fafdc47`)
+  const cidaDE = document.querySelector("input")  as HTMLInputElement
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cidaDE.value}&appid=af1f563fdce202604e28dada8fafdc47`)
   .then(response => response.json())
   .then(data => {
-    obterPaisENomeCidade(data)
-    converterTempereatura(data)
-    obterIcone(data)
+    if (validaoInput(cidaDE.value)) {
+      alert("Digite um nome de cidade")
+    }else{
+      obterPaisENomeCidade(data)
+      converterTempereatura(data)
+      obterIcone(data)
+    }
   })
   .catch(error => console.error('Erro:', error));
 };
 
-
+const button = document.querySelector("button") as HTMLButtonElement
 button.addEventListener("click", () => {
   gerarFrase()
 })
