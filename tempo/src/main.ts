239f80b4e1d8dd1e33075004c3c26188fdff381c
carlos676767 <carlitobr730@gmail.com>
2024-03-15 08:38:07 -0700

    adicionada pega bandeira do pais

--- tempo/src/main.ts
+++ tempo/src/main.ts
@@ -5,12 +5,12 @@ const converterTempereatura = (data: any) => {
     const celsus = kelvin - 273.15
     p.innerHTML = celsus.toFixed(2)
 }
-
+let sys: any
 const obterPaisENomeCidade = (data: any) => {
-    const sys = data.sys.country
+    sys = data.sys.country
     const city = data.name
     console.log(sys, city);
-    return data
+    return {data}
 }
 
 
@@ -30,14 +30,10 @@ const validaoInput = (city:string) => {
 const obterVentoAtual = (data: any) => {
   const obterVento = data.wind.speed
   console.log(obterVento);
-  
   return data
-
-  
 }
 
-
-const gerarFrase = () => {
+const gerarInformacoes = () => {
   const cidaDE = document.querySelector("input")  as HTMLInputElement
   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cidaDE.value}&appid=af1f563fdce202604e28dada8fafdc47`)
   .then(response => response.json())
@@ -50,7 +46,7 @@ const gerarFrase = () => {
       obterIcone(data)
       obterVentoAtual(data)
       console.log(data);
-      
+      obterBandeiraPais()
     }
   })
   .catch(error => console.error('Erro:', error));
@@ -58,6 +54,18 @@ const gerarFrase = () => {
 
 const button = document.querySelector("button") as HTMLButtonElement
 button.addEventListener("click", () => {
-  gerarFrase()
+  gerarInformacoes()
 })
+
+const obterBandeiraPais = () => {
+  const bandeiraPais = document.getElementById("bandeiraPais") as HTMLImageElement
+  fetch(`https://restcountries.com/v3.1/alpha/${sys}`)
+  .then(response => response.json())
+  .then(data => {
+    bandeiraPais.src = data[0].flags.png 
+  })
+  .catch(erro => {
+    console.log(erro);
+  })
+}
 
