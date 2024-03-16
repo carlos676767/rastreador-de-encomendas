643f8340b0b5fc42e3d930feadba657d260c7d9a
carlos676767 <carlitobr730@gmail.com>
2024-03-15 18:06:07 -0700

    adicionado algumas coisas

--- tempo/src/main.ts
+++ tempo/src/main.ts
@@ -1,82 +1,80 @@
-
 const converterTempereatura = (data: any) => {
-    const p = document.querySelector("p") as HTMLParagraphElement
-    const kelvin = data.main.temp
-    const celsus = kelvin - 273.15
-    p.innerHTML = celsus.toFixed(2)
-}
-let sys: any
+  const p = document.getElementById("texto") as HTMLParagraphElement;
+  const kelvin = data.main.temp;
+  const celsus = kelvin - 273.15;
+  p.innerHTML =  `<img src="" alt="icone" id="icone"> ${celsus.toFixed(2)}°C `;
+};
+let sys: any;
 const obterPaisENomeCidade = (data: any) => {
-    sys = data.sys.country
-    const city = data.name
-    console.log(sys, city);
-    return {data}
-}
-
-
-const obterIcone = (data: any) => {
-  const imagem = document.querySelector("img") as HTMLImageElement
-  const iconCode = data
-  imagem.src = iconCode
-  console.log(iconCode);
-  return data
-}
-
-
-const validaoInput = (city:string) => {
-  return city === ''
-}
-
-const obterVentoAtual = (data: any) => {
-  const obterVento = data.wind.speed
-  console.log(obterVento);
-  return data
-}
-
-const gerarInformacoes = () => {
-  const cidaDE = document.querySelector("input")  as HTMLInputElement
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cidaDE.value}&appid=af1f563fdce202604e28dada8fafdc47`)
-  .then(response => response.json())
-  .then(data => {
-    if (validaoInput(cidaDE.value)) {
-      alert("Digite um nome de cidade")
-    }else{
-      obterPaisENomeCidade(data)
-      converterTempereatura(data)
-      obterIcone(data)
-      obterVentoAtual(data)
-      console.log(data);
-      obterBandeiraPais()
-      obterTempoMinimoEmaximo(data)
-    }
-  })
-  .catch(error => console.error('Erro:', error));
+const mostrarCity = document.getElementById("mostrarCity") as HTMLParagraphElement
+  sys = data.sys.country;
+  const city = data.name;
+  mostrarCity.innerHTML = `${city}, ${sys}  <img src="" alt="bandeira" id="bandeiraP">`
+  console.log(sys, city);
+  return { data };
 };
 
-const button = document.querySelector("button") as HTMLButtonElement
+const obterIcone = (data: any) => {
+  const imagem = document.getElementById("icone") as HTMLImageElement;
+  const iconCode = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  imagem.src = iconCode;
+  return data;
+};
+
+const validaoInput = (city: string) => {
+  return city === "";
+};
+
+const obterVentoAtual = (data: any) => {
+  const obterVento = data.wind.speed;
+  console.log(obterVento);
+  return data;
+};
+
+const gerarInformacoes = () => {
+  const cidaDE = document.querySelector("input") as HTMLInputElement;
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cidaDE.value}&appid=af1f563fdce202604e28dada8fafdc47`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (validaoInput(cidaDE.value)) {
+        alert("Digite um nome de cidade");
+      } else {
+        obterPaisENomeCidade(data);
+        converterTempereatura(data);
+        obterIcone(data);
+        obterVentoAtual(data);
+        console.log(data);
+        obterBandeiraPais();
+        obterTempoMinimoEmaximo(data);
+      }
+    })
+    .catch((error) => console.error("Erro:", error));
+};
+
+const button = document.querySelector("button") as HTMLButtonElement;
 button.addEventListener("click", () => {
-  gerarInformacoes()
-})
+  gerarInformacoes();
+});
 
 const obterBandeiraPais = () => {
-  const bandeiraPais = document.getElementById("bandeiraP") as HTMLImageElement
+  const bandeiraPais = document.getElementById("bandeiraP") as HTMLImageElement;
   fetch(`https://restcountries.com/v3.1/alpha/${sys}`)
-  .then(response => response.json())
-  .then(data => {
-    bandeiraPais.src = data[0].flags.png 
-  })
-  .catch(erro => {
-    console.log(erro);
-  })
-}
+    .then((response) => response.json())
+    .then((data) => {
+      bandeiraPais.src = data[0].flags.png;
+    })
+    .catch((erro) => {
+      console.log(erro);
+    });
+};
 
- 
 const obterTempoMinimoEmaximo = (data: any) => {
-  const tempoMaximo = data.main.temp_max
-  const tempoMinimo = data.main.temp_min
-  const valorTempoMaximoEmC = tempoMaximo - 273.15
-  const valorTempoMinimoEmC = tempoMinimo - 273.15
-  
-  console.log(`tempo minimo ${valorTempoMinimoEmC}, tempo maximo ${valorTempoMaximoEmC}`);
-  return data
-}
+  const tempoMaximo = data.main.temp_max;
+  const tempoMinimo = data.main.temp_min;
+  const valorTempoMaximoEmC = tempoMaximo - 273.15;
+  const valorTempoMinimoEmC = tempoMinimo - 273.15;
+  console.log(
+    `tempo minimo ${valorTempoMinimoEmC}, tempo maximo ${valorTempoMaximoEmC}`
+  );
+  return data;
+};
